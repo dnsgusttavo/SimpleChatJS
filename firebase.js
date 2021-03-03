9be3f15945bdad5b75949a2a64bd355537e59b78
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-03 08:28:16 -0300

    fix: removed credentials and added credentials example file

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
 (function (){
 
   var firebaseConfig = {
-    apiKey: "AIzaSyCfKjHK5dlGg_L4F8eMkcLpbnezd94lZ6o",
-    authDomain: "simplechatjs.firebaseapp.com",
-    databaseURL: "https://simplechatjs.firebaseio.com",
-    projectId: "simplechatjs",
-    storageBucket: "simplechatjs.appspot.com",
-    messagingSenderId: "192291400442",
-    appId: "1:192291400442:web:7aedc32eda494b2f343743",
-    measurementId: "G-Z9KKXXHSSK"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
-      // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
-    })()
+})()
 
 
